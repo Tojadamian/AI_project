d5c71b2a923ad9bfabb41f94aa9628659f088482
igoro00 <igor.ordecha@gmail.com>
2023-10-26 23:48:43 +0200

    teraz to jest idealnie a nie to co ty miales

--- NAUKA_SIECI_NEURONOWEJ.m
+++ NAUKA_SIECI_NEURONOWEJ.m
@@ -4,13 +4,13 @@ clc;
 [P, T]=humps(0:0.05:2);
 % Definiowanie sieci wielowarstwowej uczonej metodą wstecznej propagacji błędów
 %Uczenie metodą największego spadku, 3 neurony w warstwie ukrytej
-LN=30; %Liczba neuronów
+LN=8; %Liczba neuronów
 net = newff(P,T,LN,{},'trainlm'); 
 % Definiowanie parametrów procesu uczenia - 300 epok, dopuszczalny błąd
 % 1e-5, wsp. uczenia 0.05
 net.trainParam.show = 50;
 net.trainParam.lr = 0.05;
-net.trainParam.epochs = 1000;
+net.trainParam.epochs = 100000;
 net.trainParam.goal = 1e-5;
 y = sim(net,P);
 blad=mse(T-y);
