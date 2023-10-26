2b0131db039f0a78491aaa3c3ea4e5e4b5fdfcf4
ToJaDamian <96szaman@gmail.com>
2023-10-26 23:31:03 +0200

    prosta_sieć_neuronowa(newff)

---
+++ NAUKA_SIECI_NEURONOWEJ.m
@@ -0,0 +1,24 @@
+clear;
+clc;
+% Definiowanie wektorów 1 elementowych, kolumnowych
+[P, T]=humps(0:0.05:2);
+% Definiowanie sieci wielowarstwowej uczonej metodą wstecznej propagacji błędów
+%Uczenie metodą największego spadku, 3 neurony w warstwie ukrytej
+LN=30; %Liczba neuronów
+net = newff(P,T,LN,{},'trainlm'); 
+% Definiowanie parametrów procesu uczenia - 300 epok, dopuszczalny błąd
+% 1e-5, wsp. uczenia 0.05
+net.trainParam.show = 50;
+net.trainParam.lr = 0.05;
+net.trainParam.epochs = 1000;
+net.trainParam.goal = 1e-5;
+y = sim(net,P);
+blad=mse(T-y);
+% Po procesie uczenia
+net = train(net,P,T); % Trening sieci
+y1=sim(net,P);
+figure;
+plot(T,'b');
+hold on;
+plot(y1,'r--');
+blad1=mse(y1-T);
